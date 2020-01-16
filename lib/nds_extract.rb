210b21577a4d023f626a254e796fe35738b00228
Pedro David Garcia Lopez <reddevil_cero@hotmail.com>
2020-01-16 15:12:39 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -49,6 +49,7 @@ def movies_with_director_key(name, movies_collection)
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
 
+
   array = []
 
   movies_collection.each { |movie|
