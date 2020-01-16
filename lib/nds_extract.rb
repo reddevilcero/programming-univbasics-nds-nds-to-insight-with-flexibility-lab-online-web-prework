595a189c7faf0c037f2d319cedfcd9f72a9544f0
Pedro David Garcia Lopez <reddevil_cero@hotmail.com>
2020-01-16 15:26:59 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -89,7 +89,9 @@ def movies_with_directors_set(source)
   # to have a :director_name key added to it.
   array = []
   source.each { |element|
-    puts element
+
+    array << movies_with_director_key(element[:name], element[:movies])
+    
     }
 
     array
