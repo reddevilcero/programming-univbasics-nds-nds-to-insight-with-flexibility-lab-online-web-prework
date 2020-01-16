01c1701f8f533a8e4b526c5cfa4b1146ff09c302
Pedro David Garcia Lopez <reddevil_cero@hotmail.com>
2020-01-16 14:27:12 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -21,7 +21,7 @@ def flatten_a_o_a(aoa)
 end
 
 def movie_with_director_name(director_name, movie_data)
-  {
+  { 
     :title => movie_data[:title],
     :worldwide_gross => movie_data[:worldwide_gross],
     :release_year => movie_data[:release_year],
@@ -32,14 +32,22 @@ end
 
 
 # Your code after this point
-# GOAL: For each Hash in an Array (movies_collection), provide a collection
-# of movies and a directors name to the movie_with_director_name method
-# and accumulate the returned Array of movies into a new Array that's
-# returned by this method.
-
 
 def movies_with_director_key(name, movies_collection)
-
+  # GOAL: For each Hash in an Array (movies_collection), provide a collection
+  # of movies and a directors name to the movie_with_director_name method
+  # and accumulate the returned Array of movies into a new Array that's
+  # returned by this method.
+  #
+  # INPUT:
+  # * name: A director's name
+  # * movies_collection: An Array of Hashes where each Hash represents a movie
+  #
+  # RETURN:
+  #
+  # Array of Hashes where each Hash represents a movie; however, they should all have a
+  # :director_name key. This addition can be done by using the provided
+  # movie_with_director_name method
 end
 
 
